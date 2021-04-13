6a213f8d19c1a35a1cfab728588e79546ef913cc
Lev <levdoescode@gmail.com>
2021-04-12 21:57:32 -0500

    Add Initial commit

---
+++ CM2005 Object Oriented Programming/Topic 1/1.4.1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  std::cout << "Type in a number: " << std::endl;
+  int x;
+  std::cin >> x;
+  std::cout << "You typed: " << x  << std::endl;
+  return 0;
+}
