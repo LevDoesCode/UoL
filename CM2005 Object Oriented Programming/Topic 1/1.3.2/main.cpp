96935a658101b5511b3b4fd55e6bcf73ac772bad
Lev <levdoescode@gmail.com>
2021-04-12 19:26:16 -0500

    Update rebuild

---
+++ CM2005 Object Oriented Programming/Topic 1/1.3.2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  // 1 print help
+  std::cout << "3: Make an offer " << std::endl;
+  // 2 print exchange stats
+  std::cout << "2: Print exchange stats " << std::endl;
+  // 3 make an offer
+  std::cout << "3: Make an offer " << std::endl;
+  // 4 make a bid
+  std::cout << "4: Make a bid " << std::endl;
+  // 5 print wallet
+  std::cout << "5: Print wallet " << std::endl;
+  // 6 continue
+  std::cout << "6: Continue" << std::endl;
+
+  std::cout << "==========" << std::endl;
+  std::cout << "Enter an option: " << std::endl;
+
+  int userOption;
+  std::cin >> userOption;
+  std::cout << "You chose: " << userOption  << std::endl;
+  
+  return 0;
+}
