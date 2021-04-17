d8549f4ab7f388ed6b71a4b46a6dfcf809c25a36
Lev <levdoescode@gmail.com>
2021-04-17 16:48:53 -0500

    Update updated

--- CM2030 Graphics Programming/Topic 1/1.1.5/sketch.js
+++ CM2030 Graphics Programming/Topic 1/1.1.5/sketch.js
@@ -1,11 +1,10 @@
 function setup() {
-    createCanvas(900, 600);
-    background(0);
+  createCanvas(900, 600);
+  background(0);
 }
 
-function draw()
-{
-    background(0);
-    fill(255);
-    ellipse(mouseX, mouseY, 255);
+function draw() {
+  background(0);
+  fill(255);
+  ellipse(mouseX, mouseY, 255);
 }
