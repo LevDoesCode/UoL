962d8c63e2455e8fa0414fc57377258532f00818
Lev <levdoescode@gmail.com>
2021-05-18 22:16:04 -0500

    Add Initial commit

---
+++ CM2005 Object Oriented Programming/Topic 2/2.1.11/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    double price = 5319.450228;
+    double amount = 0.00020075;
+
+    std::string timestamp{"2020/03/17 17:01:24.884492"};
+    std::string product{"BTC/USDT"};
+    //std::string orderType{"bid"};
+    enum class OrderBookType{bid, ask};
+    OrderBookType orderType = OrderBookType::ask;
+
+    return 0;
+}
