8927ae5f700bf753c54647c5a92aa3c85808b75f
Lev <levdoescode@gmail.com>
2021-05-22 23:09:34 -0500

    Add read all lines from csv file

---
+++ CM2005 Object Oriented Programming/Topic 3/3.2.3/test.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+std::vector<std::string> Tokenise(std::string csvLine, char separator)
+{
+  std::vector<std::string> tokens;
+  signed int start, end;
+  std::string token;
+  start = csvLine.find_first_not_of(separator, 0);
+  do
+  {
+    end = csvLine.find_first_of(separator, start);
+    if(start == csvLine.length() || start == end) break;
+    if(end >= 0)
+      token = csvLine.substr(start, end - start);
+    else
+      token = csvLine.substr(start, csvLine.length() - start);
+    
+    tokens.push_back(token);
+    start = end + 1;
+  } while (end > 0);
+  return tokens;
+}
+
+int main()
+{
+  std::string filename = "20200317.csv";
+  std::string line;
+  std::ifstream csvFile{filename};
+
+  if(csvFile.is_open())
+  {
+    std::cout << "File open" << std::endl;
+    while(std::getline(csvFile, line))
+    {
+      std::cout << "Read line: " << line << std::endl;
+    }
+    csvFile.close();
+  }
+  else
+  {
+    std::cout << "Could not open file" << std::endl;
+  }
+
+  return 0;
+}
