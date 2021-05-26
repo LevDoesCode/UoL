37df788f86e1ceaf2b06e3a6c5cc05497b7b10e9
Lev <levdoescode@gmail.com>
2021-05-26 02:48:08 -0500

    Add implemented getHigh and getLow

---
+++ CM2005 Object Oriented Programming/Topic 4/4.1.11/OrderBook.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "OrderBookEntry.h"
+#include "CSVReader.h"
+#include <string>
+#include <vector>
+
+class OrderBook
+{
+  public:
+    /* Construct, reading a csv data file */
+    OrderBook(std::string filename);
+    /* return vector of all known products in the dataset */
+    std::vector<std::string> getKnownProducts();
+    /* return vector of Orders according to the sent filters */
+    std::vector<OrderBookEntry> getOrders(OrderBookType type, std::string product, std::string timestamp);
+    static double getHighPrice(std::vector<OrderBookEntry>& orderList);
+    static double getLowPrice(std::vector<OrderBookEntry>& orderList);
+  private:
+    std::vector<OrderBookEntry> orders;
+};
