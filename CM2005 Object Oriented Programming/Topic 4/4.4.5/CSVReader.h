b672af9b4f21343ea89c68e13756c268b4166ce6
Lev <levdoescode@gmail.com>
2021-06-11 13:45:12 -0500

    Add second version of stringsToOBE that takes separate strings

--- CM2005 Object Oriented Programming/Topic 4/4.4.5/CSVReader.h
+++ CM2005 Object Oriented Programming/Topic 4/4.4.5/CSVReader.h
@@ -6,12 +6,16 @@
 
 class CSVReader
 {
-    public:
-     CSVReader();
-     static std::vector<OrderBookEntry> readCSV(std::string csvFile);
-     static std::vector<std::string> tokenise(std::string csvLine, char separator);
+public:
+    CSVReader();
+    static std::vector<OrderBookEntry> readCSV(std::string csvFile);
+    static std::vector<std::string> tokenise(std::string csvLine, char separator);
+    static OrderBookEntry stringsToOBE(std::string product,
+                                       std::string price,
+                                       std::string amount,
+                                       std::string timestamp,
+                                       OrderBookType orderType);
 
-    private:     
-     static OrderBookEntry stringsToOBE(std::vector<std::string> strings);
-     
-}; 
+private:
+    static OrderBookEntry stringsToOBE(std::vector<std::string> strings);
+};
