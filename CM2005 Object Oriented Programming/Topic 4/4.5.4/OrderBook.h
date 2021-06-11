dcf943bdfb977a4a5852ae15043f82b0e00787fe
Lev <levdoescode@gmail.com>
2021-06-11 18:57:29 -0500

    Update added arguments to matchAsksToBids

--- CM2005 Object Oriented Programming/Topic 4/4.5.4/OrderBook.h
+++ CM2005 Object Oriented Programming/Topic 4/4.5.4/OrderBook.h
@@ -20,7 +20,7 @@ class OrderBook
       * If no next timestamp is found, wraps to the start */
     std::string getNextTime(std::string timestamp);
     void insertOrder(OrderBookEntry& order);
-    std::vector<OrderBookEntry> matchAsksToBids();
+    std::vector<OrderBookEntry> matchAsksToBids(std::string product, std::string timestamp);
     static double getHighPrice(std::vector<OrderBookEntry>& orderList);
     static double getLowPrice(std::vector<OrderBookEntry>& orderList);
   private:
