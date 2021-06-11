e4df71bccec4dc8f1b7341dbb244c6002e01c111
Lev <levdoescode@gmail.com>
2021-06-11 13:47:18 -0500

    Update changed order of parameters

--- CM2005 Object Oriented Programming/Topic 4/4.4.5/CSVReader.h
+++ CM2005 Object Oriented Programming/Topic 4/4.4.5/CSVReader.h
@@ -10,10 +10,10 @@ public:
     CSVReader();
     static std::vector<OrderBookEntry> readCSV(std::string csvFile);
     static std::vector<std::string> tokenise(std::string csvLine, char separator);
-    static OrderBookEntry stringsToOBE(std::string product,
-                                       std::string price,
+    static OrderBookEntry stringsToOBE(std::string price,
                                        std::string amount,
                                        std::string timestamp,
+                                       std::string product,
                                        OrderBookType orderType);
 
 private:
