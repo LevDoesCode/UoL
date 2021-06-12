028aec9a1daab4d355ce649a1f1142ec83e4deed
Lev <levdoescode@gmail.com>
2021-06-12 10:54:37 -0500

    Update fixed the price comparison

--- CM2005 Object Oriented Programming/Topic 4/4.5.8/OrderBookEntry.h
+++ CM2005 Object Oriented Programming/Topic 4/4.5.8/OrderBookEntry.h
@@ -2,36 +2,40 @@
 
 #include <string>
 
-enum class OrderBookType{bid, ask, unknown, sale};
+enum class OrderBookType
+{
+    bid,
+    ask,
+    unknown,
+    sale
+};
 
 class OrderBookEntry
 {
-    public:
+public:
+    OrderBookEntry(double _price,
+                   double _amount,
+                   std::string _timestamp,
+                   std::string _product,
+                   OrderBookType _orderType);
 
-        OrderBookEntry( double _price, 
-                        double _amount, 
-                        std::string _timestamp, 
-                        std::string _product, 
-                        OrderBookType _orderType);
+    static OrderBookType stringToOrderBookType(std::string s);
+    static bool compareByTimestamp(OrderBookEntry &entry1, OrderBookEntry &entry2)
+    {
+        return entry1.timestamp < entry2.timestamp;
+    }
+    static bool compareByPriceAsc(OrderBookEntry &entry1, OrderBookEntry &entry2)
+    {
+        return entry1.price < entry2.price;
+    }
+    static bool compareByPriceDesc(OrderBookEntry &entry1, OrderBookEntry &entry2)
+    {
+        return entry1.price > entry2.price;
+    }
 
-        static OrderBookType stringToOrderBookType(std::string s);
-        static bool compareByTimestamp(OrderBookEntry& entry1, OrderBookEntry& entry2)
-        {
-            return entry1.timestamp < entry2.timestamp;
-        }
-        static bool compareByPriceAsc (OrderBookEntry& entry1, OrderBookEntry& entry2)
-        {
-            return entry1.timestamp < entry2.timestamp;
-        }
-        static bool compareByPriceDesc (OrderBookEntry& entry1, OrderBookEntry& entry2)
-        {
-            return entry1.timestamp > entry2.timestamp;
-        }
-
-        double price;
-        double amount;
-        std::string timestamp;
-        std::string product;
-        OrderBookType orderType;
+    double price;
+    double amount;
+    std::string timestamp;
+    std::string product;
+    OrderBookType orderType;
 };
- 
