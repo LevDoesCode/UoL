ae736454ecd6b35e3565f7d325813a34b4e92eb5
Lev <levdoescode@gmail.com>
2021-06-12 22:19:11 -0500

    Add Initial commit to integrate to the main app enterAsk

---
+++ CM2005 Object Oriented Programming/Topic 5/5.1.15/Wallet.cpp
@@ -0,0 +1,90 @@
+#include "Wallet.h"
+#include "CSVReader.h"
+
+Wallet::Wallet()
+{
+
+}
+
+void Wallet::insertCurrency(std::string type, double amount)
+{
+  double balance;
+  if(amount < 0)
+  {
+    throw std::exception{};
+  }
+  if(currencies.count(type) == 0) // not in wallet yet
+  {
+    balance = 0;
+  }
+  else
+  {
+    balance = currencies[type];
+  }
+  balance += amount;
+  currencies[type] = balance;
+}
+
+bool Wallet::removeCurrency(std::string type, double amount)
+{
+  double balance;
+  if(amount < 0)
+  {
+    return false;
+  }
+  if(currencies.count(type) == 0) // currency not in wallet
+  {
+    return false;
+  }
+  else // is there, do we have enough?
+  {
+    if(containsCurrency(type, amount))
+    {
+      currencies[type] -= amount;
+      return true;
+    }
+    else // didn't have enough
+      return false;
+  }
+}
+
+bool Wallet::containsCurrency(std::string type, double amount)
+{
+  if(currencies.count(type) == 0) // not in wallet yet
+  {
+    return false;
+  }
+  else
+    return currencies[type] >= amount;
+}
+
+std::string Wallet::toString()
+{
+  std::string s;
+  for(std::pair<const std::string, double> pair : currencies)
+  {
+    std::string currency = pair.first;
+    double amount = pair.second;
+    s += currency + " : " + std::to_string(amount) + "\n";
+  }
+  return s;
+}
+
+bool Wallet::canFulfillOrder(OrderBookEntry order)
+{
+  std::vector<std::string> currs = CSVReader::tokenise(order.product, '/');
+  // ask
+  if(order.orderType == OrderBookType::ask)
+  {
+    double amount = order.amount;
+    std::string currency = currs[0];
+    return containsCurrency(currency, amount);
+  }
+  if(order.orderType == OrderBookType::bid)
+  {
+    double amount = order.amount * order.price;
+    std::string currency = currs[1];
+    return containsCurrency(currency, amount);
+  }
+  return false;
+}
---
+++ CM2005 Object Oriented Programming/Topic 5/5.1.15/Wallet.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include "OrderBookEntry.h"
+
+class Wallet
+{
+  public:
+    Wallet();
+    /** insert currency to the wallet */
+    void insertCurrency(std::string type, double amount);
+
+    /** remove currency from the wallet */
+    bool removeCurrency(std::string type, double amount);
+
+    /** check if the wallet contains at least this much currency */
+    bool containsCurrency(std::string type, double amount);
+
+    /** check if the wallet can cope with the ask or bid */
+    bool canFulfillOrder(OrderBookEntry order);
+
+    /** generate a string representation of the wallet */
+    std::string toString();
+
+  private:
+    std::map<std::string, double> currencies;
+};
