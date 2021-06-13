28e8407e6b67e2c7089a2cb3b37b7b3a9324c1d7
Lev <levdoescode@gmail.com>
2021-06-12 19:15:27 -0500

    Update implemented insert and contains functions

--- CM2005 Object Oriented Programming/Topic 5/5.1.7/Wallet.cpp
+++ CM2005 Object Oriented Programming/Topic 5/5.1.7/Wallet.cpp
@@ -7,12 +7,31 @@ Wallet::Wallet()
 
 void Wallet::insertCurrency(std::string type, double amount)
 {
-
+  double balance;
+  if(amount < 0)
+  {
+    throw std::exception{};
+  }
+  if(currencies.count(type) == 0) // not in wallet yet
+  {
+    balance = 0;
+  }
+  else
+  {
+    balance = currencies[type];
+  }
+  balance += amount;
+  currencies[type] = balance;
 }
 
 bool Wallet::containsCurrency(std::string type, double amount)
 {
-  return false;
+  if(currencies.count(type) == 0) // not in wallet yet
+  {
+    return false;
+  }
+  else
+    return currencies[type] >= amount;
 }
 
 std::string Wallet::toString()
