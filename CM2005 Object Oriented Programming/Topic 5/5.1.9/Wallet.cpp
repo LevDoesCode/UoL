5b092f9e4a940abd02e46bbb20571c6673445b3a
Lev <levdoescode@gmail.com>
2021-06-12 21:02:14 -0500

    Update changed std::pair to const

--- CM2005 Object Oriented Programming/Topic 5/5.1.9/Wallet.cpp
+++ CM2005 Object Oriented Programming/Topic 5/5.1.9/Wallet.cpp
@@ -60,7 +60,7 @@ bool Wallet::containsCurrency(std::string type, double amount)
 std::string Wallet::toString()
 {
   std::string s;
-  for(std::pair<std::string, double> pair : currencies)
+  for(std::pair<const std::string, double> pair : currencies)
   {
     std::string currency = pair.first;
     double amount = pair.second;
