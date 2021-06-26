bae4e12969739adfbb7144208ddb7961cda967bb
Lev <levdoescode@gmail.com>
2021-06-26 18:26:06 -0500

    Update random color and size

--- CM2030 Graphics Programming/Topic 6/6.1.1 randomDots/sketch.js
+++ CM2030 Graphics Programming/Topic 6/6.1.1 randomDots/sketch.js
@@ -11,7 +11,8 @@ function draw() {
     var randX = random(0, width);
     var randY = random(0, height);
 
-    fill(random(255), 0, 0, random(255));
+    fill(random(50, 255), random(50, 255), random(50, 255), random(255));
     noStroke();
-    ellipse(randX, randY, 15, 15);
+    let size = random(15, 25);
+    ellipse(randX, randY, size, size);
 }
