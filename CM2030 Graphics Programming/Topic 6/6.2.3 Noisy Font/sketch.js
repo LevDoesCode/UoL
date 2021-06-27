895935ec36dbccaf2de6fad2f91ea0ad71ada067
Lev <levdoescode@gmail.com>
2021-06-26 23:38:11 -0500

    Update completed

--- CM2030 Graphics Programming/Topic 6/6.2.3 Noisy Font/sketch.js
+++ CM2030 Graphics Programming/Topic 6/6.2.3 Noisy Font/sketch.js
@@ -1,10 +1,11 @@
-var points;
-var font;
+let points;
+let font;
+let time = 0;
+let amt = 20;
 function preload() {
   font = loadFont('assets/Calistoga-Regular.ttf');
 }
 
-//////////////////////////////////////////////////////////////////////
 function setup() {
   createCanvas(900, 400);
   background(0);
@@ -15,10 +16,21 @@ function setup() {
   });
 }
 
-//////////////////////////////////////////////////////////////////////
 function draw() {
-    fill(0,5);
-    rect(0,0,width,height);
+  background(0);
+  noStroke();
+  fill(255, 104, 204, 150);
 
-    // **** Your code here ****
+  amt = map(mouseX, 0, width, 0, 20);
+  // **** Your code here ****
+  for (let i = 0; i < points.length; i++)
+  {
+    let nX = noise(frameCount%width + points[i].x);
+    let deltaX = map(nX, 0, 1, -amt, amt);
+    
+    let nY = noise(frameCount%width + points[i].y);
+    let deltaY = map(nY, 0, 1, -amt, amt);
+    //console.log(nX);
+    ellipse(points[i].x + deltaX, points[i].y + deltaY, 10, 10);
+  }
 }
