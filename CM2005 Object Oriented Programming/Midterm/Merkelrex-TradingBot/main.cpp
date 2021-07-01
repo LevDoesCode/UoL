bfb805e93d826b3910d4aa163888748bf5df2b2f
Lev <levdoescode@gmail.com>
2021-07-01 16:52:41 -0500

    Update created trading bot variable and sent reference of merkel to it

--- CM2005 Object Oriented Programming/Midterm/Merkelrex-TradingBot/main.cpp
+++ CM2005 Object Oriented Programming/Midterm/Merkelrex-TradingBot/main.cpp
@@ -1,10 +1,11 @@
 #include "Wallet.h"
 #include <iostream>
 #include "MerkelMain.h"
+#include "TradingBot.h"
 
 int main()
 {   
     MerkelMain app{};
+    TradingBot bot{&app};
     app.init();
-    
 }
