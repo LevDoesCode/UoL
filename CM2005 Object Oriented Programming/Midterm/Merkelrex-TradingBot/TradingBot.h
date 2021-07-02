656ed9d71df3b9947402b9c57f7ea4282dd4b041
Lev <levdoescode@gmail.com>
2021-07-02 17:14:35 -0500

    Update ask and bid books retrieved from Merkel

--- CM2005 Object Oriented Programming/Midterm/Merkelrex-TradingBot/TradingBot.h
+++ CM2005 Object Oriented Programming/Midterm/Merkelrex-TradingBot/TradingBot.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include "MerkelMain.h" // required to interact with the order book, place bids, withdraw bids, place asks
+#include "OrderBookEntry.h" // required to build vectors of this type
+#include <string>
+#include <iostream>
+#include <vector>
 
 class TradingBot
 {
   public:
     TradingBot(MerkelMain* merkel);
+    void testFunc();
   private:
     MerkelMain* merkel;
+    std::vector<OrderBookEntry> bookBids;
+    std::vector<OrderBookEntry> bookAsks;
 };
