719d34529173664c11b96604a73dece45ad7248f
Lev <levdoescode@gmail.com>
2021-07-02 17:24:59 -0500

    Update implemented getCurrentAsks and getCurrentBids

--- CM2005 Object Oriented Programming/Midterm/Merkelrex-TradingBot/MerkelMain.h
+++ CM2005 Object Oriented Programming/Midterm/Merkelrex-TradingBot/MerkelMain.h
@@ -5,28 +5,40 @@
 #include "OrderBook.h"
 #include "Wallet.h"
 
-
 class MerkelMain
 {
-    public:
-        MerkelMain();
-        /** Call this to start the sim */
-        void init();
-    private: 
-        void printMenu();
-        void printHelp();
-        void printMarketStats();
-        void enterAsk();
-        void enterBid();
-        void printWallet();
-        void gotoNextTimeframe();
-        int getUserOption();
-        void processUserOption(int userOption);
+public:
+    MerkelMain();
+    /** Call this to start the sim */
 
-        std::string currentTime;
+    /** Returns the current time */
+    std::string getCurrentTime();
+    
+    /** Returns the list of known products */
+    std::vector<std::string> getKnownProducts();
+    
+    /** returns a vector of all asks orders for the current time */
+    std::vector<OrderBookEntry> getCurrentAsks();
 
-        OrderBook orderBook{"20200317.csv"};
-    	//OrderBook orderBook{"20200601.csv"};
-        Wallet wallet;
+    /** returns a vector of all bids orders for the current time */
+    std::vector<OrderBookEntry> getCurrentBids();
 
+    void enterAsk();
+private:
+    void printMenu();
+    void printHelp();
+    void printMarketStats();
+
+    void enterBid();
+    void printWallet();
+    void gotoNextTimeframe();
+    int getUserOption();
+    void init(); // initialization function for user interaction
+    void processUserOption(int userOption);
+
+    std::string currentTime;
+
+    OrderBook orderBook{"20200317.csv"};
+    //OrderBook orderBook{"20200601.csv"};
+    Wallet wallet;
 };
