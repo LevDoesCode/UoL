b093e30ccdfcec70b5033261ccd4d1100d456ae9
Lev <levdoescode@gmail.com>
2021-07-16 00:02:10 -0500

    Update completed and cleared line

--- CM2030 Graphics Programming/Topic 8/8.1.8 Polar Roses/sketch.js
+++ CM2030 Graphics Programming/Topic 8/8.1.8 Polar Roses/sketch.js
@@ -1,6 +1,12 @@
+let d = 8
+let n = 5;
+let sliderk;
+
 function setup()
 {
   createCanvas(500, 500);
+  sliderd = createSlider(1, 10, 5);
+  slidern = createSlider(1, 10, 5)
   background(0);
 }
 
@@ -10,16 +16,21 @@ function draw()
   translate(width/2, height/2);
 
   stroke(255);
-  noStroke();
+  stroke(255);
+  noFill();
+
+  n = slidern.value();
+  d = sliderd.value();
+  k = n/d;
 
   beginShape();
-  for (let angle=0; angle < TWO_PI; angle += 0.02)
+  for (let angle=0; angle < TWO_PI * 10; angle += 0.02)
   {
-    let r = 200 * cos(7 * angle);
+    let r = 200 * cos(k * angle);
     let x = r * cos(angle);
     let y = r * sin(angle);
     
     vertex(x, y);
   }
-  endShape(CLOSE);
+  endShape();
 }
