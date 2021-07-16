a0200fc21f2b3ac4bf8400ab8bcb52812a82edba
Lev <levdoescode@gmail.com>
2021-07-16 00:51:59 -0500

    Update completed roses with sliders

--- CM2030 Graphics Programming/Topic 8/8.1.8 Maurer Roses/sketch.js
+++ CM2030 Graphics Programming/Topic 8/8.1.8 Maurer Roses/sketch.js
@@ -1,13 +1,15 @@
 let n = 6;
 let d = 71;
-let sliderk;
+let sliderd;
+let slidern;
 
 function setup()
 {
   createCanvas(500, 500);
   angleMode(DEGREES);
-  sliderd = createSlider(1, 10, 5);
-  slidern = createSlider(1, 10, 5);
+  sliderd = createSlider(1, 180, 1);
+  slidern = createSlider(1, 180, 1);
+  //sliderd.input(draw);
   background(0);
 }
 
@@ -16,7 +18,10 @@ function draw()
   background(0);
   translate(width/2, height/2);
   stroke(255);
-  noFill();
+  noFill(); 
+  strokeWeight(1);
+  d = sliderd.value();
+  n = slidern.value();
 
   beginShape();
   for(let i = 0; i <= 360; i++)
@@ -27,5 +32,20 @@ function draw()
     let y = r * sin(k);
     vertex(x, y);
   }
-  endShape(CLOSE);
+  endShape();
+
+  noFill();
+  stroke(255, 0, 255);
+  strokeWeight(4);
+  beginShape();
+  for(let i = 0; i <= 360; i++)
+  {
+    let k = i;
+    let r = 200 * sin(n*k);
+    let x = r * cos(k);
+    let y = r * sin(k);
+    vertex(x, y);
+  }
+  endShape();
+  //noLoop();
 }
