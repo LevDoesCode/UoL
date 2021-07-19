6b51977c6cea15ce9aef7ca6a915374a8a0ed9af
Lev <levdoescode@gmail.com>
2021-07-18 19:05:19 -0500

    Update complted 3D system intro

--- CM2030 Graphics Programming/Topic 11/11.1.2 Intro 3D graphics/sketch.js
+++ CM2030 Graphics Programming/Topic 11/11.1.2 Intro 3D graphics/sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-
+  createCanvas(500, 500, WEBGL);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  
+  background(125);
+  rectMode(CENTER);
+  //rotateX(frameCount);
+  rotateY(frameCount);
+  translate(200, 0, 0);
+  //rotateZ(frameCount);
+  rect(0, 0, 100, 100);
+  box(75);
 }
