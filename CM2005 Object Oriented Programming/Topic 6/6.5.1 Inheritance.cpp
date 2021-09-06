ca67a8d998bc777d2af92dc5da15dd8bf4435364
Lev <levdoescode@gmail.com>
2021-09-05 19:13:41 -0500

    Add inheritance example

---
+++ CM2005 Object Oriented Programming/Topic 6/6.5.1 Inheritance.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+class Vehicle
+{
+  public:
+    Vehicle();
+    float getspeed();
+    void accelerate();
+    void beepHorn();
+  private:
+    float speed;
+};
+
+class Train : public Vehicle
+{
+  public:
+    Train();
+    void beepHorn();
+};
+
+Train::Train()
+{
+
+}
+
+void Train::beepHorn()
+{
+  if(getspeed() < 0.5)
+  {
+    std::cout << "bip" << std::endl;
+  }
+  else
+  {
+    std::cout << "BOOP BIP" << std::endl;
+  }
+}
+
+Vehicle::Vehicle() : speed(0.0f)
+{
+
+}
+
+float Vehicle::getspeed()
+{
+  return speed;
+}
+
+void Vehicle::accelerate()
+{
+  speed += 0.5f;
+}
+
+void Vehicle::beepHorn()
+{
+  std::cout << "meep meep" << std::endl;
+}
+
+int main()
+{
+  Vehicle v;
+  v.beepHorn();
+
+  Train t;
+  t.beepHorn();
+  t.accelerate();
+  t.beepHorn();
+}
